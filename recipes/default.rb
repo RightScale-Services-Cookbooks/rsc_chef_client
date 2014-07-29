6d8788de2328bbe1559154b06c076c9212f36a44
Alex Pop <alex@rightscale.com>
2014-07-29 16:08:22 +0100

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,4 +6,6 @@
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-rightscale_marker
+marker "recipe_start_rightscale" do
+  template "rightscale_audit_entry.erb"
+end
