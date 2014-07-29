a39804b23d5bd852905cad00a14dc371d27a4075
Alex Pop <alex@rightscale.com>
2014-07-29 16:08:09 +0100

    Update do_client_converge.rb

--- recipes/do_client_converge.rb
+++ recipes/do_client_converge.rb
@@ -6,7 +6,9 @@
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-rightscale_marker
+marker "recipe_start_rightscale" do
+  template "rightscale_audit_entry.erb"
+end
 
 log "  Current Chef Client role(s) are: #{node[:chef][:client][:current_roles]}"
 
