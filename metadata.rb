9fb64f0c4f54da0292fcb5f993d4f28ef998eb3f
Alex Pop <al3xpop@gmail.com>
2014-07-31 13:44:02 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "support@rightscale.com"
 license          "Copyright RightScale, Inc. All rights reserved."
 description      "Installs and configures Chef Client"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "14.0.0"
+version          "14.0.1"
 
 supports "centos"
 supports "redhat"
